8451cc151dad8671d151f62b58acf0379c616078
LDDestroier <etheilig02@gmail.com>
2019-02-11 21:01:33 -0500

    Update lddterm-demo.lua

--- lddterm-demo.lua
+++ lddterm-demo.lua
@@ -26,7 +26,7 @@ term.write("This is a test of 'LDDTerm'!", 4, 7)
 sleep(0.8)
 
 term.setBackgroundColorRGB(255, 0, 0)
-term.setTextColorRGB(255, 255, 255)
+term.setTextColorRGB(0x000000)
 
 term.write("It's an API that provides basic \"windows\" that you can write in.", 5, 9)
 sleep(0.5)
@@ -73,5 +73,21 @@ term.write("Supports 'term.scroll' and 'term.scrollX',", 10, 13)
 term.write("amongst other things!", 10, 14)
 
 sleep(1)
+
+for i = 1, 120 do
+	term.setTextColorRGB(math.random(0, 0xFFFFFF))
+	term.setBackgroundColorRGB(math.random(0, 0xFFFFFF))
+	term.write((" "):rep(math.random(0, 12)) .. "Fuck you")
+end
+
+sleep(1)
+
+term.setTextColor(colors.white)
+term.setBackgroundColor(colors.black)
+
+for i = 1, 8 do
+	term.scroll(1)
+	sleep(0.05)
+end
 
 term.write("Look in the code to see what the functions!", 4, 16)
