56876496503f74feab089c6b0b768ce82539ebe5
LDDestroier <etheilig02@gmail.com>
2019-02-12 19:30:33 -0500

    Added spawning windows and moveWindow

--- lddterm-demo.lua
+++ lddterm-demo.lua
@@ -1,93 +1,126 @@
 local lddterm = dofile("lddterm.lua")
-local window = lddterm.newWindow(72, 16, 4, 4)
-local term = window.handle
-local colors = window.ccapi.colors
+
+local baseSize = {46, 12}
 
 lddterm.alwaysRender = true
 
-local sleep = lddterm.sleep
 
-local i = 0
+drawDemo = function(window)
+	lddterm.setLayer(window, 1)
+	local term = window.handle
+	local colors = window.ccapi.colors
 
-term.clear(".")
+	local sleep = lddterm.sleep
 
-sleep(1)
+	local i = 0
 
-term.setBackgroundColor(colors.red)
-term.setTextColor(colors.yellow)
+	term.setBackgroundColorRGB(0x262626)
+	term.clear(".")
 
-term.write("Hello!", 9, 5)
-sleep(0.5)
+	sleep(1)
 
-term.setBackgroundColor(colors.blue)
-term.setTextColor(colors.red)
+	term.setBackgroundColor(colors.blue)
+	term.setTextColor(colors.yellow)
 
-term.write("This is a test of 'LDDTerm'!", 4, 7)
-sleep(0.8)
+	term.write("Hello!", 9, 5)
+	sleep(0.5)
 
-term.setBackgroundColorRGB(255, 0, 0)
-term.setTextColorRGB(0x000000)
+	term.setBackgroundColor(colors.orange)
+	term.setTextColorRGB(0x0000FF)
 
-term.write("It's an API that provides basic \"windows\" that you can write in.", 5, 9)
-sleep(0.5)
+	term.write("This is a test of 'LDDTerm'!", 4, 7)
+	sleep(0.8)
 
-term.write("similar to the ComputerCraft's own term and window APIs!", 6, 10)
-sleep(1)
+	term.setBackgroundColorRGB(255, 0, 0)
+	term.setTextColorRGB(0x000000)
 
-term.setBackgroundColor(colors.black)
-term.setTextColor(colors.white)
+	term.write("It's an API that provides basic \"windows\" that you can write in.", 5, 9)
+	sleep(0.5)
 
-for i = 1, 5 do
-	term.scroll(-1)
-	sleep(0.05)
+	term.write("similar to the ComputerCraft's own term and window APIs!", 6, 10)
+	sleep(1)
+
+	term.setBackgroundColorRGB(0x262626)
+	term.setTextColor(colors.white)
+
+	for i = 1, 3 do
+		term.scroll(-1)
+--		sleep(0.05)
+	end
+
+	for i = 1, 3 do
+		term.scrollX(-1)
+--		sleep(0.05)
+	end
+
+	for i = 1, 6 do
+		term.scroll(1)
+--		sleep(0.05)
+	end
+
+	for i = 1, 6 do
+		term.scrollX(1)
+--		sleep(0.05)
+	end
+
+	for i = 1, 3 do
+		term.scroll(-1)
+--		sleep(0.05)
+	end
+
+	for i = 1, 3 do
+		term.scrollX(-1)
+--		sleep(0.05)
+	end
+
+	sleep(1)
+
+	for i = 1, 5 do
+		term.scroll(1)
+--		sleep(0.05)
+	end
+
+	term.write("Supports 'term.scroll' and 'term.scrollX',", 2, 6)
+	term.write("amongst other things!", 2, 7)
+	term.write("You can also move windows.", 2, 9)
+
+	sleep(1)
+
+	for i = 1, 4 do
+		term.moveWindow(window.x + 1, window.y)
+	end
+
+	for i = 1, 8 do
+		term.moveWindow(window.x - 1, window.y)
+	end
+
+	for i = 1, 4 do
+		term.moveWindow(window.x + 1, window.y)
+	end
+
+	for i = 1, 3 do
+		term.moveWindow(window.x, window.y - 1)
+	end
+
+	for i = 1, 6 do
+		term.moveWindow(window.x, window.y + 1)
+	end
+
+	for i = 1, 3 do
+		term.moveWindow(window.x, window.y - 1)
+	end
+
+	sleep(1)
+
+	for i = 1, 100 do
+		term.setTextColorRGB(math.random(0, 0xFFFFFE))
+		term.setBackgroundColorRGB(math.random(0, 0xFFFFFF))
+		term.write((" "):rep(math.random(0, 12)) .. "Fuck you")
+	end
+
+	sleep(1)
 end
 
-for i = 1, 5 do
-	term.scrollX(-1)
-	sleep(0.05)
+for i = 1, 1024 do
+	drawDemo(lddterm.newWindow(baseSize[1], baseSize[2], math.random(1, 20), math.random(1, 12)))
 end
-
-for i = 1, 10 do
-	term.scroll(1)
-	sleep(0.05)
-end
-
-for i = 1, 15 do
-	term.scrollX(1)
-	sleep(0.05)
-end
-
-for i = 1, 5 do
-	term.scroll(-1)
-	sleep(0.05)
-end
-
-for i = 1, 5 do
-	term.scrollX(-1)
-	sleep(0.05)
-end
-
-sleep(1)
-
-term.write("Supports 'term.scroll' and 'term.scrollX',", 10, 13)
-term.write("amongst other things!", 10, 14)
-
-sleep(1)
-
-for i = 1, 120 do
-	term.setTextColorRGB(math.random(0, 0xFFFFFF))
-	term.setBackgroundColorRGB(math.random(0, 0xFFFFFF))
-	term.write((" "):rep(math.random(0, 12)) .. "Fuck you")
-end
-
-sleep(1)
-
-term.setTextColor(colors.white)
-term.setBackgroundColor(colors.black)
-
-for i = 1, 8 do
-	term.scroll(1)
-	sleep(0.05)
-end
-
-term.write("Look in the code to see what the functions!", 4, 16)
